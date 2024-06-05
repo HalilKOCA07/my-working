62bba12895225fce0bac65439169ca6e61bc3ece
Sinyour <sinyour52@gmail.com>
2024-06-05 23:32:57 +0300

    drawer and navbar done

---
+++ react/post-class/01-stock-app/src/components/DashboardList.jsx
@@ -0,0 +1,123 @@
+import React from "react";
+import Divider from "@mui/material/Divider";
+import Toolbar from "@mui/material/Toolbar";
+import {
+    Avatar,
+  Box,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Menu,
+  MenuItem,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import DashboardCustomizeIcon from "@mui/icons-material/DashboardCustomize";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import EuroIcon from "@mui/icons-material/Euro";
+import BusinessIcon from "@mui/icons-material/Business";
+import BookmarksIcon from "@mui/icons-material/Bookmarks";
+import Inventory2Icon from "@mui/icons-material/Inventory2";
+import { useNavigate } from "react-router-dom";
+
+const DashboardList = () => {
+    const navigate = useNavigate()
+  const icons = [
+    {
+      title: "Dashboard",
+      icon: <DashboardCustomizeIcon />,
+      path: "/stock",
+    },
+    {
+      title: "Purchases",
+      icon: <ShoppingCartIcon />,
+      path: "/stock/purchases",
+    },
+    {
+      title: "Sales",
+      icon: <EuroIcon />,
+      path: "/stock/sales",
+    },
+    {
+      title: "Firms",
+      icon: <BusinessIcon />,
+      path: "/stock/firms",
+    },
+    {
+      title: "Brands",
+      icon: <BookmarksIcon />,
+      path: "/stock/brands",
+    },
+    {
+      title: "Products",
+      icon: <Inventory2Icon />,
+      path: "/stock/products",
+    },
+  ];
+  return (
+    <div>
+      <Toolbar />
+      <Divider />
+      <List>
+        {icons.map((item, index) => (
+          <ListItem key={item.title} disablePadding>
+            <ListItemButton sx={{color:"white"}}onClick={()=> navigate(item.path)}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.title} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+};
+
+export default DashboardList;
+
+
+        //todo **********  NAVLIST  ****************
+export const NavList = () => {
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+    const settings = ['Profile', 'Logout'];
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+      };
+      const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+      };
+  return (
+    <Box sx={{ flexGrow: 0 }}>
+    <Tooltip title="Open settings">
+      <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+        <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+      </IconButton>
+    </Tooltip>
+    <Menu
+      sx={{ mt: '45px' }}
+      id="menu-appbar"
+      anchorEl={anchorElUser}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={Boolean(anchorElUser)}
+      onClose={handleCloseUserMenu}
+    >
+      {settings.map((setting) => (
+        <MenuItem key={setting} onClick={handleCloseUserMenu}>
+          <Typography textAlign="center">{setting}</Typography>
+        </MenuItem>
+      ))}
+    </Menu>
+  </Box>
+  )
+}
+
--- react/post-class/01-stock-app/src/router/AppRouter.jsx
+++ react/post-class/01-stock-app/src/router/AppRouter.jsx
@@ -4,6 +4,12 @@ import Login from "../pages/Login";
 import PrivaterRouter from "./PrivaterRouter";
 import Dashboard from "../pages/Dashboard";
 import Register from "../pages/Register";
+import Purchases from "../pages/Purchases";
+import Sales from "../pages/Sales";
+import Firms from "../pages/Firms";
+import Home from "../pages/Home";
+import Brands from "../pages/Brands";
+import Products from "../pages/Products";
 
 const AppRouter = () => {
   return (
@@ -12,7 +18,14 @@ const AppRouter = () => {
         <Route path="/" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="/stock" element={<PrivaterRouter />}>
-          <Route path="" element={<Dashboard />} />
+          <Route path="" element={<Dashboard />}>
+            <Route index element={<Home />} />
+            <Route path="/stock/purchases" element={<Purchases />} />
+            <Route path="/stock/sales" element={<Sales />} />
+            <Route path="/stock/firms" element={<Firms />} />
+            <Route path="/stock/brands" element={<Brands />} />
+            <Route path="/stock/products" element={<Products />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
